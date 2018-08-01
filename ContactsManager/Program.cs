47b24b9f86af693f105473372c3ff90812a6f254
YBAZAN <40167348+YBAZAN@users.noreply.github.com>
2018-08-01 17:12:44 +0200

    Réalisation du menu et des méthodes (TODO)

--- ContactsManager/Program.cs
+++ ContactsManager/Program.cs
@@ -8,31 +8,86 @@ namespace ContactsManager
 {
     class Program
     {
-        static List<string> contacts = new List<string>();
+        List<string> contacts = new List<string>();
 
         static void Main(string[] args)
         {
             AfficherMenu();
+
         }
 
         static void AfficherMenu()
         {
+            Console.Clear();
 
+            Console.WriteLine("MENU\n");
+            Console.WriteLine("1. Liste des contacts");
+            Console.WriteLine("2. Ajout d’un contact");
+            Console.WriteLine("3. Suppression d’un contact");
+            Console.WriteLine("4. Quitter");
+            Console.Write("\nVotre choix: ");
+
+            var choix = Console.ReadLine();
+            switch (choix)
+            {
+                case "1":
+                    ListerContacts();
+                    break;
+                case "2":
+                    AjouterContact();
+                    break;
+                case "3":
+                    SupprimerContact();
+                    break;
+                case "4":
+                    Quitter();
+                    break;
+                default:
+                    Console.WriteLine("Choix invalide, l'application va fermer");
+                    Quitter();
+                    break;
+            }
         }
 
         static void ListerContacts()
         {
+            Console.Clear();
+            Console.WriteLine("LISTE DES CONTACTS\n");
 
+            Console.WriteLine("TODO...");
+
+            Console.WriteLine("\nAppuyez sur une touche pour revenir au menu...");
+            Console.ReadKey();
+            AfficherMenu();
         }
 
-        static void AjouterContacts()
+        static void AjouterContact()
         {
+            Console.Clear();
+            Console.WriteLine("AJOUT D'UN CONTACT\n");
 
+            Console.WriteLine("TODO...");
+
+            Console.WriteLine("\nAppuyez sur une touche pour revenir au menu...");
+            Console.ReadKey();
+            AfficherMenu();
         }
 
-        static void SupprimerContacts()
+        static void SupprimerContact()
         {
+            Console.Clear();
+            Console.WriteLine("SUPPRESSION D'UN CONTACT\n");
 
+            Console.WriteLine("TODO...");
+
+            Console.WriteLine("\nAppuyez sur une touche pour revenir au menu...");
+            Console.ReadKey();
+            AfficherMenu();
+        }
+
+        static void Quitter()
+        {
+            Environment.Exit(0);
         }
     }
 }
